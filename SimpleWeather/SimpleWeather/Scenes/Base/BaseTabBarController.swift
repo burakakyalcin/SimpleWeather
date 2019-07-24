fbfe9d66fc1aa1eb219bcd5b3b37452c5377ed19
Akyalcin, Burak <burak.akyalcin@accenture.com>
2019-07-24 13:31:06 +0300

    linked outlets

--- SimpleWeather/SimpleWeather/Scenes/Base/BaseTabBarController.swift
+++ SimpleWeather/SimpleWeather/Scenes/Base/BaseTabBarController.swift
@@ -13,7 +13,7 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tabBar.unselectedItemTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.tabBar.unselectedItemTintColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         self.tabBar.tintColor = #colorLiteral(red: 0.2224404216, green: 0.6473951936, blue: 1, alpha: 1)
     }
     
